af0ec9db435fee27f2d7e3038d82cbcc097afb55
xinyueDang <xinyue.dang@algodepth.com>
2021-03-09 17:11:50 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import {BrowserRouter, Route} from "react-router-dom";
 import Home from "./components/home"
+import RecipeProfile from "./components/recipe-profile/recipe-profile";
 
 function App() {
   return (
       <BrowserRouter>
         <div className="container-fluid">
-            <Route path="/" exact={true}>
+            <Route path="/" exact>
                 <Home/>
+            </Route>
+            <Route path="/recipe/recipe-profile" exact>
+              <RecipeProfile/>
             </Route>
         </div>
       </BrowserRouter>
---
+++ src/components/recipe-profile/ingredients.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Ingredients = () => {
+
+}
+
+export default Ingredients
