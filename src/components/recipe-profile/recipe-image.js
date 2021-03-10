e512d5642c3b4b2919131049fdd4ebe9c915e747
xinyueDang <xinyue.dang@algodepth.com>
2021-03-09 21:34:56 -0500

    Create static recipe-profile

--- src/components/recipe-profile/recipe-image.js
+++ src/components/recipe-profile/recipe-image.js
@@ -11,11 +11,11 @@ const RecipeImage = () => {
         className="carousel slide image-display"
         data-bs-ride="carousel"
       >
-        <ol class="carousel-indicators">
+        <ol className="carousel-indicators">
           <li
             data-target="#carouselExampleIndicators"
             data-slide-to="0"
-            class="active"
+            className="active"
           ></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         </ol>
@@ -28,22 +28,22 @@ const RecipeImage = () => {
           </div>
         </div>
         <a
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           href="#carouselExampleIndicators"
           role="button"
           data-slide="prev"
         >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
         </a>
         <a
-          class="carousel-control-next"
+          className="carousel-control-next"
           href="#carouselExampleIndicators"
           role="button"
           data-slide="next"
         >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
         </a>
       </div>
     </>
--- src/components/recipe-profile/recipe-profile.js
+++ src/components/recipe-profile/recipe-profile.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link, Route } from "react-router-dom";
 import "./recipe-profile.css";
-import RecipeImage from "./recipe-image"
+import RecipeImage from "./recipe-image";
+import Ingredients from "./ingredients";
+import Directions from "./directions";
+import Reviews from "./reviews"
 
 const RecipeProfile = () => {
   return (
@@ -11,22 +14,27 @@ const RecipeProfile = () => {
       </div>
       <div className="row">
         <div className="col-2">
-            <i className="fas fa-star"></i>
-            <i className="fas fa-star"></i>
-            <i className="fas fa-star"></i>
-            <i className="fas fa-star"></i>
-            <i className="far fa-star"></i>
+          <i className="fas fa-star"></i>
+          <i className="fas fa-star"></i>
+          <i className="fas fa-star"></i>
+          <i className="fas fa-star"></i>
+          <i className="far fa-star"></i>
         </div>
         <div className="col-3">
-            <a>Author Name</a>
+          <a>Author Name</a>
         </div>
         <div className="col-7 collect-op">
-            <i className="far fa-heart fa-lg"></i>
-            &nbsp;
-            <i className="far fa-star fa-lg"></i>
+          <i className="far fa-heart fa-lg"></i>
+          &nbsp;
+          <i className="far fa-star fa-lg"></i>
         </div>
       </div>
-      <RecipeImage/>
+      <RecipeImage />
+      <Ingredients />
+      &nbsp;
+      <Directions />
+      &nbsp;
+      <Reviews />
     </div>
   );
 };
