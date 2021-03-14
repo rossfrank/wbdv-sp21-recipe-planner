71fe1b200e620eca8193ffcca9eecf78a7a936cb
rossfrank <rossfrank27@gmail.com>
2021-03-14 13:34:54 -0400

    profile cards

--- src/components/profile/profile-tabs.js
+++ src/components/profile/profile-tabs.js
@@ -24,7 +24,6 @@ const ProfileTabs = ({}) => {
                                 </Link>
                             </li>
                         </div>
-
                     )
                 }
             </ul>
--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -2,6 +2,7 @@ import React from 'react'
 import UserDescription from "./user-description";
 import "./profile.css";
 import ProfileTabs from "./profile-tabs";
+import ProfileCards from "./profile-cards";
 
 export default class Profile extends React.Component {
 
@@ -14,11 +15,12 @@ export default class Profile extends React.Component {
             <div>
                 <h1>Profile</h1>
                 <div className="row">
-                    <div className="col-4">
+                    <div className="col-2">
                         <UserDescription />
                     </div>
                     <div className="col-8">
                         <ProfileTabs/>
+                        <ProfileCards/>
                     </div>
                 </div>
             </div>
