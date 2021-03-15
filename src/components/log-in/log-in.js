ec5081661e58132b2ff64a62e08d4777c4389846
xinyueDang <xinyue.dang@algodepth.com>
2021-03-15 11:12:35 -0400

    Adjust the margin with nav-bar

--- src/components/log-in/log-in.js
+++ src/components/log-in/log-in.js
@@ -3,28 +3,30 @@ import "./log-in.css";
 
 const LogIn = () => {
   return (
-    <div className="a-box percentage40-item">
+    <div className="login-page">
+      <div className="a-box percentage40-item">
         <div className="a-box-inner">
-      <h3>Log In</h3>
-      <form>
-        <div className="form-group">
-          <label>Email</label>
-          <input className="form-control"/>
+          <h3>Log In</h3>
+          <form>
+            <div className="form-group">
+              <label>Email</label>
+              <input className="form-control" />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input className="form-control" />
+            </div>
+            <div className="form-group">
+              <a className="btn btn-primary btn-block" href="">
+                Log In
+              </a>
+            </div>
+            <div className="form-group URL-link">
+              Do not have an account?
+              <a href="/signup">Create an account</a>
+            </div>
+          </form>
         </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input className="form-control"/>
-        </div>
-        <div className="form-group">
-            <a className="btn btn-primary btn-block" href="">
-              Log In
-            </a>
-        </div>
-        <div className="form-group URL-link">
-            Do not have an account?
-            <a href="/signup">Create an account</a>
-          </div>
-      </form>
       </div>
     </div>
   );
