c22b41a5550a834cc1ae6ac9a75207fe83405780
AilinShen <aln67911@gmail.com>
2021-04-09 23:07:34 -0400

    user registration

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,12 +1,25 @@
 function UserService(){
 
     this.userLogin = userLogin;
+    this.userRegister = userRegister;
+    this.url = "https://wbdv-recipe-planner-server.herokuapp.com"
+    const self = this
 
     function userLogin(userCredential){
-        return fetch("https://wbdv-recipe-planner-server.herokuapp.com/login", {
+        return fetch(`${self.url}/login`, {
             method: "POST",
             headers: {'content-type': 'application/json'},
             body: JSON.stringify(userCredential)
+        }).then((res)=>{
+            return res.json()
+        })
+    }
+
+    function userRegister(user){
+        return fetch(`${self.url}/register`, {
+            method: "POST",
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify(user)
         }).then((res)=>{
             return res.json()
         })
