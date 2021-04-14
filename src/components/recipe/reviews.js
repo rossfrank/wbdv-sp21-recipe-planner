cc0e66704e72be6d3ccc29e73ec01323913f1089
XinyueDang <sinreydang@gmail.com>
2021-04-13 20:32:52 -0400

    Add Favorite and Review handlers

--- src/components/recipe/reviews.js
+++ src/components/recipe/reviews.js
@@ -2,20 +2,20 @@ import React, { useEffect, useState } from "react";
 import "./recipe-profile.css";
 import User from "./pic/user.jpeg";
 import { connect } from "react-redux";
-import reviewService from "../../services/review-service"
+import reviewService from "../../services/review-service";
 import { useParams } from "react-router";
 
 const Reviews = ({
   reviews = [],
   createReview,
   findReviewsForRecipe,
-  findReviewsForUser
+  findReviewsForUser,
 }) => {
-    const { recipeId } = useParams()
-    useEffect(() => {
-      findReviewsForRecipe(recipeId)
-  }, [recipeId])
-  console.log(reviews)
+  const { recipeId } = useParams();
+  const [newReview, setNewReview] = useState("");
+  useEffect(() => {
+    findReviewsForRecipe(recipeId);
+  }, [recipeId]);
   return (
     <>
       <div className="row center-element percentage70-item">
@@ -24,44 +24,40 @@ const Reviews = ({
         </div>
       </div>
       <ul className="percentage70-item center-element">
-        <div className="media">
-          <img
-            src={User}
-            className="align-self-start mr-3 user-photo"
-            alt="..."
-          />
-          <div className="media-body">
-            <a className="mt-0" href="/profile">
-              User1
-            </a>
-            <p>This dish is healthy and super delicious.</p>
-          </div>
-        </div>
-
-        <div className="media">
-          <img
-            src={User}
-            className="align-self-start mr-3 user-photo"
-            alt="..."
-          />
-          <div className="media-body">
-            <a className="mt-0" href="/profile">
-              User2
-            </a>
-            <p>
-              I love this dish so much. I love this dish so much. I love this
-              dish so much. I love this dish so much. I love this dish so much.
-            </p>
-          </div>
-        </div>
+        {reviews.map((review) => {
+          return (
+            <div key={review.reviewId}>
+              <div className="media">
+                <img
+                  src={User}
+                  className="align-self-start mr-3 user-photo"
+                  alt="..."
+                />
+                <div className="media-body">
+                  <a className="mt-0" href="/profile">
+                    {review.userId}
+                  </a>
+                  <p>{review.text}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </ul>
       <div className="percentage70-item center-element">
         <textarea
           placeholder="Please leave your review here."
           className="form-control"
+          onChange={(e) => setNewReview(e.target.value)}
+          value={newReview}
         ></textarea>
         <br />
-        <button className="btn btn-warning float-right">Submit</button>
+        <button
+          className="btn btn-warning float-right"
+          onClick={() => createReview(recipeId, newReview)}
+        >
+          Submit
+        </button>
       </div>
     </>
   );
@@ -75,11 +71,11 @@ const stpm = (state) => {
 
 const dtpm = (dispatch) => {
   return {
-    createReview: (recipeId) => {
+    createReview: (recipeId, newReview) => {
       reviewService
         .createReview(recipeId, {
           userId: "1",
-          text: "New Review",
+          text: newReview,
         })
         .then((theReview) =>
           dispatch({
@@ -88,6 +84,7 @@ const dtpm = (dispatch) => {
           })
         );
     },
+    
     findReviewsForRecipe: (recipeId) =>
       reviewService.findReviewForRecipe(recipeId).then((theReviews) =>
         dispatch({
