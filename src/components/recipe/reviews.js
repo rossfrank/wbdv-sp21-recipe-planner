5e21f49c76dd246fed023d4210fbf978b03b9e38
AilinShen <aln67911@gmail.com>
2021-04-23 22:28:30 -0400

    fix merge

--- src/components/recipe/reviews.js
+++ src/components/recipe/reviews.js
@@ -87,9 +87,6 @@ const dtpm = (dispatch) => {
       reviewService
         .createReview(recipeId, {
           text: newReview,
-          user: {
-              id: user.userId,
-          },
           user: {id: user.userId},
           recipeName: recipe,
         })
