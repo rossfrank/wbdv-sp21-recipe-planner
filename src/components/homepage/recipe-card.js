679abf85f0d208db0b121c8a0a819f6545ca64cb
AilinShen <aln67911@gmail.com>
2021-04-23 22:15:59 -0400

    add user contents on home

--- src/components/homepage/recipe-card.js
+++ src/components/homepage/recipe-card.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './recipe-card.css'
 import {Link} from "react-router-dom";
+import Ingredients from "../recipe/ingredients";
 
 
 function RecipeCard({recipe={}}) {
@@ -8,18 +9,30 @@ function RecipeCard({recipe={}}) {
     return(
         <div className="col mb-4">
             <div className="card h-100">
-                <img src={recipe.image} className="card-img-top" alt="..."/>
+                <img src={recipe.image} className="card-img-top" alt="..."  width="500" height="270"/>
                 <div className="card-body">
                     <Link className="card-title"
                           to={`/details/${recipe.id}`}>
                         {recipe.title}
                     </Link>
                     <h6 className="card-subtitle">Ingredients</h6>
-                    <div className="card-summary">
-                        {recipe.extendedIngredients !== undefined &&
-                        recipe.extendedIngredients.map(ingredient=>{return `${ingredient.name}, `})
-                        }
-                    </div>
+                    {
+                        (recipe["id"].toString().substring(0, 3)!=="rcp") &&
+                        <div className="card-summary">
+                            {recipe.extendedIngredients !== undefined &&
+                            recipe.extendedIngredients.map(ingredient=>{return `${ingredient.name}, `})
+                            }
+                        </div>
+                    }
+                    {
+                        (recipe["id"].toString().substring(0, 3) ==="rcp") &&
+                        <div className="card-summary">
+                            {recipe.ingredientList !== undefined &&
+                            recipe.ingredientList.map(ingredient=>{return `${ingredient.name}, `})
+                            }
+                        </div>
+                    }
+
                     <span className="card-timer">
                         <i className="far fa-clock " />
                         {recipe.readyInMinutes} min
